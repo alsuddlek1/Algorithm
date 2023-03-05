6fca9829590b8591729b52c7d9095c55b0b44a3a
minyoung <minyoung99311@gmail.com>
2023-03-06 01:13:02 +0900

    1860

--- 1860_진기의 최고급 붕어빵/prac.py
+++ 1860_진기의 최고급 붕어빵/prac.py
@@ -6,14 +6,15 @@ for tc in range(1, T+1):
     N, M, K = map(int,input().split())
     # N명의 사람, M초당 K개의 붕어빵
     data = list(map(int, input().split()))
-    dataa = sorted(data)
+    dataa = sorted(data) # 사람들 방문하는 시간 정렬
 
-    people = 0
+    result = 'Possible'
+    people = 0 # 한명당 붕어빵 하나씩 줘야함
     for i in dataa:
         people += 1
         bread = (i//M) * K
 
-    if bread < people:
-        print(f'#{tc}', 'impossible')
-    else:
-        print(f'#{tc}', 'possible')
+        if people > bread:
+            result = 'Impossible'
+            break
+    print(f'#{tc} {result}')
