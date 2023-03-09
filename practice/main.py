f662b0e064ac6e70681a9c5b28651609da83be5a
minyoung <minyoung99311@gmail.com>
2023-03-09 21:43:33 +0900

    pra

--- practice/main.py
+++ practice/main.py
@@ -20,13 +20,58 @@ sys.stdin = open('input.txt')
 #         idx = i+1
 # print(maxV, idx)
 
-A = int(input())
-B = int(input())
-C = int(input())
+# A = int(input())
+# B = int(input())
+# C = int(input())
+#
+# data = A*B*C
+# d_list = list(str(data))
+# # print(d_list)
+# matrix = [0] * 10
+# for i in range(10):
+#     print(d_list.count(str(i)))
 
-data = A*B*C
-d_list = list(str(data))
-# print(d_list)
-matrix = [0] * 10
-for i in range(10):
-    print(d_list.count(str(i)))
+
+# A, B = map(int,input().split())
+#
+# print(A+B)
+# print(A-B)
+# print(A*B)
+# print(A//B)
+# print(A%B)
+#
+# arr = input()
+# print(f'{arr}??!')
+
+# A = int(543)
+# b_year = int(input())
+#
+# print(b_year - A)
+
+#10430
+# A, B, C = map(int, input().split())
+# print((A+B) % C)
+# print(((A % C)+(B % C)) % C)
+# print((A*B) % C)
+# print(((A % C)*(B % C)) % C)
+
+# A = int(input())
+# B = input()
+# # print(A,B)
+#
+# mul_1 = A * int(list(str(B))[-1])
+# mul_2 = A * int(list(str(B))[1])
+# mul_3 = A * int(list(str(B))[0])
+# print(mul_1)
+# print(mul_2)
+# print(mul_3)
+# print(A * int(B))
+#
+# A,B,C = map(int,input().split())
+# print(A+B+C)
+
+print('|\_/|')
+print("|q p|   /}")
+print('( 0 )"""\\')
+print('|"^"`    |')
+print('||_/=\\\\__|')
