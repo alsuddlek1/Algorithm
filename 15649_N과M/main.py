aee2af226d6f8caab5844731046c517f640e6d67
minyoung <minyoung99311@gmail.com>
2023-04-13 00:24:11 +0900

    2309

--- 15649_N과M/main.py
+++ 15649_N과M/main.py
@@ -1,11 +1,21 @@
 import sys
 sys.stdin = open('input.txt')
 
-def aa(i, x):
-    if i == N:
-        
+def dfs(n, lst):
+    if n == M:
+        ans.append(lst)
+        return
 
+    for j in range(1, N+1):
+        if v[j] == 0:
+            v[j] = 1
+            dfs(n+1, lst+[j])
+            v[j] = 0
 
 N, M = map(int, input().split())
-arr = [i for i in range(1, N+1)]
-sset = set()
+ans = []
+v = [0] * (N+1)
+
+dfs(0, [])
+for lst in ans:
+    print(*lst)
