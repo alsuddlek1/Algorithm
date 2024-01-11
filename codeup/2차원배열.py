c1f66a64bae460ba7423bf85ac9822f92d401a61
minyoung <minyoung99311@gmail.com>
2024-01-11 12:43:21 +0900

    codeup|1461

--- codeup/2차원배열.py
+++ codeup/2차원배열.py
@@ -1,8 +1,16 @@
 n = int(input())
+
+result = [[0]*(n+1) for _ in range(n+1)]
 c = 1
 
 for i in range(1, n+1):
-    for j in range(1, n+1):
-        print(c, end=' ')
+    for j in range(n, 0, -1):
+        print(i, j)
+        result[i][j] = c
+        print(result)
         c += 1
+
+for x in range(1, n+1):
+    for y in range(1, n+1):
+        print(result[x][y], end=' ')
     print()
