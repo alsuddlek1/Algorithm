63f47cd3bcc7970d6f577a4c1511ff2f0bc75ce5
minyoung <minyoung99311@gmail.com>
2024-01-29 15:16:42 +0900

    TCT|99

---
+++ thiscote/99_1이 될때까지.py
@@ -0,0 +1,14 @@
+N, K = map(int, input().split())
+result = 0
+while True:
+    target = (N // K)*K
+    result += (N-target)
+    N = target
+
+    if N < K:
+        break
+    result += 1
+    N //= K
+
+result += (N-1)
+print(result)
