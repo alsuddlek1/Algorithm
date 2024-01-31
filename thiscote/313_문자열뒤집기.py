4f9797d6fd59adfdc80dffa4bbf050a223388641
minyoung <minyoung99311@gmail.com>
2024-01-31 16:14:48 +0900

    TCT|313

--- thiscote/313_문자열뒤집기.py
+++ thiscote/313_문자열뒤집기.py
@@ -1,21 +1,18 @@
-pro_data = input()
-data = []
-for _ in pro_data:
-    data.append(int(_))
+data = input()
 
 count_0 = 0
 count_1 = 0
 
-if data[0] == 0: # 전부 0으로 바꾸기
-    count_1 = 0
+if data[0] == "1": # 전부 0으로 바꾸기
+    count_0 += 1
 else: # 전부 1로 바꾸기
-    count_0 = 0
+    count_1 += 1
 
 for i in range(len(data)-1):
     if data[i] != data[i+1]:
-        if data[i+1] == 0:
-            count_1 += 1
-        else:
+        if data[i+1] == "1":
             count_0 += 1
+        else:
+            count_1 += 1
 
-print(min(count_1, count_0))
+print(min(count_0, count_1))
