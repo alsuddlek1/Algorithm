e6d22e8c622b486d1080ea0b631e061e382481a9
minyoung <minyoung99311@gmail.com>
2024-01-31 16:41:02 +0900

    TCT|315_풀이2 추가

--- thiscote/315_볼링공 고르기.py
+++ thiscote/315_볼링공 고르기.py
@@ -9,3 +9,19 @@ for i in range(N):
             result += 1
 
 print(result)
+
+#  교재 풀이
+
+# n,m = map(int, input().split())
+# data = list(map(int, input().split()))
+#
+# array = [0] * 11
+# for x in data:
+#     array[x] += 1
+#
+# result = 0
+# for i in range(1, m-1):
+#     n -= array[i]
+#     result += array[i] * n
+#
+# print(result)
