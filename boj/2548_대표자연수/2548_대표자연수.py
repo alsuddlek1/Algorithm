5e3e3e34487576d9bd7f64f770bd61f2a4367111
minyoung <minyoung99311@gmail.com>
2024-05-08 00:03:46 +0900

    240508.py

--- boj/2548_대표자연수/2548_대표자연수.py
+++ boj/2548_대표자연수/2548_대표자연수.py
@@ -1,30 +1,42 @@
-# N = int(input())
-# data = list(map(int, input().split()))
-#
-# result = []
-# max_value = 10000
-#
-# for i in range(N):
-#     num = 0
-#     for j in range(N):
-#         num += abs(data[j] - data[i])
-#     if max_value >= num:
-#         max_value = num
-#         result.append(data[i])
-#
-# print(min(result))
+## 반복문을 활용한 풀이
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+data = list(map(int, input().split()))
+
+result = []
+max_value = 10000
+
+for i in range(N):
+    num = 0
+    for j in range(N):
+        num += abs(data[j] - data[i])
+    if max_value >= num:
+        max_value = num
+        result.append(data[i])
+
+print(min(result))
+
+
+## 중앙값을 이용한 풀이
+import sys
+input = sys.stdin.readline
 
 N = int(input())
 data = list(map(int, input().split()))
 data.sort()
 
 median = data[N // 2]
-print(median)
 
-result = []
 num = 0
 for x in data:
     num += abs(x-median)
-    result.append(x)
 
-print(result)
+for i in range(N):
+    cnt = 0
+    for j in range(N):
+        cnt += abs(data[j] - data[i])
+    if cnt == num:
+        print(data[i])
+        break
