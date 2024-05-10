a4fad6bcdf248dc46ba1147e2f39e601a5318be6
minyoung <minyoung99311@gmail.com>
2024-05-10 23:15:28 +0900

    240510.py

---
+++ programmers/68644_두 개 뽑아서 더하기.py
@@ -0,0 +1,15 @@
+def solution(numbers):
+    answer = []
+    n = len(numbers)  # 5
+    # 0,1,2,3,4
+
+    c = 0
+    for i in range(n):
+        for j in range(i + 1, n):
+            c = numbers[i] + numbers[j]
+            if c not in answer:
+                answer.append(c)
+
+    answer.sort()
+
+    return answer
