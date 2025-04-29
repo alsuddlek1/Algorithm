cec1f13337c03ee68bfd45b79255fe17c0fa1bff
민영 <122499580+alsuddlek1@users.noreply.github.com>
2025-04-29 13:51:22 +0900

    [Silver II] Title: 숫자판 점프, Time: 916 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/2210. 숫자판 점프/숫자판 점프.py
+++ 백준/Silver/2210. 숫자판 점프/숫자판 점프.py
@@ -1,17 +1,14 @@
-# 0. 변수설정
 data = [list(map(int, input().split())) for _ in range(5)]
 nums = []
 
-# 1. dfs
+## dfs
 def in_range(x, y):
     return 0 <= x < 5 and 0 <= y < 5
 
-def dfs(x, y, lis):
-    # print(lis)
-    if len(lis) == 6:
-        if lis not in nums:
-            nums.append(lis)
-            # print(nums)
+def dfs(x, y, arr):
+    if len(arr) == 6:
+        if arr not in nums:
+            nums.append(arr)
         return
 
     dxs = [-1, 1, 0, 0]
@@ -21,12 +18,14 @@ def dfs(x, y, lis):
         nx = x + dx
         ny = y + dy
         if in_range(nx, ny):
-            dfs(nx, ny, lis + [data[nx][ny]])
+            dfs(nx, ny, arr + [data[nx][ny]])
 
-# 2. dfs 실행
+    return x, y
+
+## dfs 실행
 for i in range(5):
     for j in range(5):
         dfs(i, j, [data[i][j]])
 
+## 정답
 print(len(nums))
-
