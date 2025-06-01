09441a51656d573f4ac11782a422f3039b0aba86
민영 <122499580+alsuddlek1@users.noreply.github.com>
2025-06-01 23:42:13 +0900

    [Silver IV] Title: 치킨치킨치킨, Time: 56 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/16439. 치킨치킨치킨/치킨치킨치킨.py
@@ -0,0 +1,13 @@
+from itertools import combinations
+
+n, m = map(int, input().split())
+like = [list(map(int, input().split())) for _ in range(n)]
+
+maxSum = 0
+for a, b, c in combinations(range(m), 3):
+    tmpSum = 0
+    for i in range(n):
+        tmpSum += max(like[i][a], like[i][b], like[i][c])
+    maxSum = max(maxSum, tmpSum)
+
+print(maxSum)
